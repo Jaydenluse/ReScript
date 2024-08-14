ad193d9352056e8d667bcaecf28edeea11a4e231
Jayden Luse <lusejaydenc@johndeere.com>
2024-08-14 15:26:54 -0500

    chore: redeploy

--- example_project/vite.config.ts
+++ example_project/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/re_script/'
+  base: '/re_script/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
